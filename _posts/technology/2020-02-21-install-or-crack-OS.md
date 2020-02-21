0c8ac06dd2af60009198f82a262b2796a9b32df4
Rongwei Wang <wangrongwei2014@gmail.com>
2020-02-22 00:02:14 +0800

    Site updated: 2020-02-22 00:02:12

---
+++ _posts/technology/2020-02-21-install-or-crack-OS.md
@@ -0,0 +1,29 @@
+---
+layout: article
+title:  "安装win10"
+date:   2019-02-21
+categories: tech
+toc: true
+image:
+    teaser: teaser/win10.png
+---
+
+> step by step 安装win10
+
+## install
+
+- 下载ISO镜像
+- 制作U盘启动盘
+
+制作U盘启动盘，可以使用**UltraISO**。
+
+## crack
+
+首先需要
+
+- 关闭防火墙
+- 关闭实时保护
+
+接下来，可以使用KMS进行crack
+
+>后续：无意想起来，下次装系统补充上。
--- new_post.sh
+++ new_post.sh
@@ -1,15 +1,43 @@
 #! /bin/sh
 
-title=$1
+class=$1
+title=$2
+
+suffix=".md"
+
+if [[ ! $class ]]
+then
+    echo "Please input a class for post"
+    exit 
+fi
+
 if [[ ! $title ]]
 then
     echo "Please input a title for post"
     exit 
 fi
 
-title=`octopress new post "$title" `
-mv $title _drafts/
-filename=${title#*/_posts/}
-echo "New Draft created as: _drafts/$filename"
-echo "_drafts/$filename" | pbcopy  
+
+filename=$(date "+%Y-%m-%d-")
+filename=$filename$title$suffix
+
+echo $filename
+
+dir="_posts/"$class"/"
+
+#判断文件夹是否存在
+if [ ! -d $dir ]; 
+then
+    echo "Please input a right class"
+    echo "$class ""is't exist"
+    exit
+fi
+
+filepath=$dir$filename
+echo $filepath
+
+touch $filepath
+
+echo "New blog created as: $filename"
+
 
